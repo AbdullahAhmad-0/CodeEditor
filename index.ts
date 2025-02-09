7516f7905f54f2803f2ff7fb2d3e51e29979a3e8
Abdullah Ahmad <abdullah.devloper@gmail.com>
2025-02-09 13:08:14 +0500

    basic command terminals

--- index.ts
+++ index.ts
@@ -1,21 +1,44 @@
 import { WebContainer } from "@webcontainer/api";
 
+const form: HTMLElement | null = document.getElementById("editor");
+const output: HTMLElement | null = document.getElementById("output");
+
 window.addEventListener("load", async () => {
     console.log("Loading...");
     const container = await WebContainer.boot();
     console.log("Container booted");
 
-    const response = await container.spawn("node", ["-v"]);
+    
+    async function run(command: string, args: string[]) {
+        const response = await container.spawn(command, args);
+        if (await response.exit) {
+            console.log("Hurray! You got error");
+        }
 
-    if (await response.exit) {
-        console.log("Hurray! You got error");
+        response.output.pipeTo(
+            new WritableStream({
+                write(chunk) {
+                    if (output) {
+                        output.innerText += (chunk + "\n");
+                    }
+                },
+            })
+        );
     }
 
-    response.output.pipeTo(new WritableStream({
-        write(chunk) {
-            console.log(chunk);
-        }
-    }));
+    // await run("node", ["-v"]);
+    // await run("py", ["-v"]);
 
-    console.log("Done");
+    form?.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        if (!e.target) {return;}
+        const data = new FormData(e.target as HTMLFormElement);
+
+        const text = data.get("command") as string;
+        console.log(text);
+        const [command, ...args] = (text || "").split(" ");
+        await run(command, args);
+    })
+
 });
